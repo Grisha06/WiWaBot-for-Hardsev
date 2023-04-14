5a7a36f22c439fd053806f025ba1a99e4e2d2621
NezertorcheaT <tigrisha06ps@gmail.com>
2023-04-14 20:42:52 +0300

    ass

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
-import random
+import json
+import subprocess
 
-import discord
-from discord.ext import commands
+import PloudosAPI
+import disnake
+from disnake.ext import commands
+
+session = PloudosAPI.login("NezertorcheaT", "Batman06psps")
+server = session.get_server(0)
 
 config = {
     'token': 'MTA5NjE1ODg2NDM4NDIxMzEwNA.GtUtuZ.mqq44K_idActK5XtSTEbKPGJ0Lk4ZFUJuwzHSs',
-    'prefix': '$',
+    'prefix': '!',
     'id': 1096158864384213104,
 }
 
-bot = commands.Bot(command_prefix=config['prefix'],intents=discord.Intents.default())
+bot = commands.Bot(command_prefix=config['prefix'], help_command=None, intents=disnake.Intents.all())
 
 
 @bot.command()
-async def rand(ctx, *arg):
-    await ctx.reply(random.randint(0, 100))
+async def start_server(ctx: commands.Context, *arg):
+    try:
+        server.start()
+    except Exception as e:
+        print(repr(e))
+        await ctx.reply(repr(e))
+    finally:
+        await ctx.reply("Done!")
+
+
+@bot.command()
+async def force_stop(ctx: commands.Context, *arg):
+    s = json.loads(server.force_stop())
+
+    if s.get('error', True) is False:
+        await ctx.reply('is done')
+    else:
+        await ctx.reply(
+            f'something wrong with sopping **{server.serverName}** server...\ntry to read error message:\n`{s.get("errorText")}`')
 
 
 bot.run(config['token'])
